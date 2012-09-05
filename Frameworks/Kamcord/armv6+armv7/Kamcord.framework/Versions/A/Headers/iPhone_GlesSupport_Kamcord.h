b663f20269917c9569a86bd4dbd81eb4e5d8a54e
Kevin Wang <kev.y.wang@gmail.com>
2012-09-05 01:17:41 -0700

    Added Frameworks

---
+++ Frameworks/Kamcord/armv6+armv7/Kamcord.framework/Versions/A/Headers/iPhone_GlesSupport_Kamcord.h
@@ -0,0 +1,14 @@
+#ifndef _KAMCORD_IPHONE_GLESSUPPORT_H_
+#define _KAMCORD_IPHONE_GLESSUPPORT_H_
+
+#import "iPhone_GlesSupport.h"
+#import <CoreGraphics/CoreGraphics.h>
+
+void CreateSurfaceGLES_Kamcord(EAGLSurfaceDesc* surface, void * context, void * parentViewController);
+void DestroySurfaceGLES_Kamcord(EAGLSurfaceDesc* surface);
+void CreateSurfaceMultisampleBuffersGLES_Kamcord(EAGLSurfaceDesc* surface);
+void DestroySurfaceMultisampleBuffersGLES_Kamcord(EAGLSurfaceDesc* surface);
+void PreparePresentSurfaceGLES_Kamcord(EAGLSurfaceDesc* surface);
+void AfterPresentSurfaceGLES_Kamcord(EAGLSurfaceDesc* surface);
+
+#endif
