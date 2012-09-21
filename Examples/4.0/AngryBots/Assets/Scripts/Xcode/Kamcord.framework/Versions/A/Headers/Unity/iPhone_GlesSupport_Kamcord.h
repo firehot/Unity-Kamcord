7b0a7a2ce1ad7960594a2b726eebc7549d0bac3f
Kevin Wang <kev.y.wang@gmail.com>
2012-09-21 02:55:23 -0700

    Added 3.5.5. and 4.0

---
+++ Examples/4.0/AngryBots/Assets/Scripts/Xcode/Kamcord.framework/Versions/A/Headers/Unity/iPhone_GlesSupport_Kamcord.h
@@ -0,0 +1,13 @@
+#ifndef _KAMCORD_IPHONE_GLESSUPPORT_H_
+#define _KAMCORD_IPHONE_GLESSUPPORT_H_
+
+#import "iPhone_GlesSupport.h"
+
+void CreateSurfaceGLES_Kamcord(EAGLSurfaceDesc * surface, void * context, void * parentViewController);
+void CreateRenderingSurfaceGLES_Kamcord(EAGLSurfaceDesc * surface);
+void DestroySurfaceGLES_Kamcord(EAGLSurfaceDesc * surface);
+void DestroyRenderingSurfaceGLES_Kamcord(EAGLSurfaceDesc * surface);
+void PreparePresentSurfaceGLES_Kamcord(EAGLSurfaceDesc * surface);
+void AfterPresentSurfaceGLES_Kamcord(EAGLSurfaceDesc * surface);
+
+#endif
